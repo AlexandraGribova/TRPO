e86fa70e3494020a03a8339bf21d43ec98fc1b89
Грибова Александра Романовна <98461789+AlexandraGribova@users.noreply.github.com>
2023-10-01 23:22:00 +0700

    Add input and output

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,39 @@
 import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Operations operations = new Operations();
-        float v1_point1[] = new float[2], v1_point2[] = new float[2];//Координаты 1 вектора
-        float v2_point1[] = {1, 1}, v2_point2[] = {2, 2};//Координаты 2 вектора
-        Scanner in = new Scanner(System.in);
-        // Ввод координат вектора1
-        System.out.printf("Введите координаты вектора.\nПервая точка: ");
-        for (int i=0; i<2; i++)
-            v1_point1[i] = in.nextFloat();
-        System.out.printf("Вторая точка: ");
-        for (int i=0; i<2; i++)
-            v1_point2[i] = in.nextFloat();
+        String line;
+        float[][] vectors = new float[4][2];
+        // считывание из файла
+        try {
+            FileInputStream fis = new FileInputStream("C:/Users/sasha/IdeaProjects/project1/src/input.txt");
+            InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
+            BufferedReader br = new BufferedReader(isr);
+            for(int j=0; (line=br.readLine())!=null; j++)
+            {
+                String[] str = line.split(" ");
+                for(int i=0; i<str.length; i++)
+                    vectors[j][i] = Float.parseFloat(str[i]);
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
         // Создание двух векторов
-        Vector vec1 = new Vector(v1_point1[0], v1_point1[1], v1_point2[0], v1_point2[1]);
-        Vector vec2 = new Vector(1, 1, 2, 2);
-        // Через класс opertions производятся действия с векторами
-        operations.getSumm(vec1, vec2);
+        Vector vec1 = new Vector(vectors[0][0],vectors[0][1], vectors[1][0], vectors[1][1]);
+        Vector vec2 = new Vector(vectors[2][0], vectors[2][1], vectors[3][0], vectors[3][1]);
+        Vector vec_result = new Vector();
+        // Операции с векторами
+        vec_result = operations.getSum(vec1, vec2);
+        // Вывод в файл
+        try (FileWriter writer = new FileWriter("output.txt")) {
+            writer.write("Результат суммы векторов:\n");
+            writer.write(Float.toString(vec_result.vec_x));
+            writer.write(" ");
+            writer.write(Float.toString(vec_result.vec_y));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/Vector.java
+++ src/Vector.java
@@ -7,13 +7,13 @@ public class Vector {
     float vec_x, vec_y;
 
     // Конструктор для инициализации вектора по координате вектора
-    Vector(float vec_x, float vec_y)
+    public Vector(float vec_x, float vec_y)
     {
         this.vec_x = vec_x;
         this.vec_y = vec_y;
     }
     // Конструктор для инициализации вектора по координатам двух точек
-    Vector(float point1_x,float point1_y, float point2_x, float point2_y)
+    public Vector(float point1_x,float point1_y, float point2_x, float point2_y)
     {
         this.point1_x = point1_x;
         this.point1_y = point1_y;
@@ -21,6 +21,11 @@ public class Vector {
         this.point2_y = point2_y;
         coordinate();
     }
+
+    public Vector() {
+
+    }
+
     // нахождение координат вектора по его точкам
     private void coordinate(){
         this.vec_x = this.point2_x - this.point1_x;
